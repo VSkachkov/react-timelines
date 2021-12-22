386211fa334636647747cad01da3b8f5b97989c6
Vitalii Skachkov <vitalii.skachkov@asml.com>
2021-12-22 15:04:43 +0100

    Updates

---
+++ precache-manifest.9e9c291d9236b1339ef35ea194ed25fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05190c236910d933189d6033e9d4b3b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "117a9afcd063b4a5157f",
+    "url": "/static/css/2.eb92054a.chunk.css"
+  },
+  {
+    "revision": "8e08023bed8cc5fafd11",
+    "url": "/static/css/main.c024e31b.chunk.css"
+  },
+  {
+    "revision": "117a9afcd063b4a5157f",
+    "url": "/static/js/2.ed42a0a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed42a0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e08023bed8cc5fafd11",
+    "url": "/static/js/main.60a38150.chunk.js"
+  },
+  {
+    "revision": "a896469b35ab17db43f7",
+    "url": "/static/js/runtime-main.126eb33a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a0036f451c571a5ae3985b3588451c6.js"
+  "/precache-manifest.9e9c291d9236b1339ef35ea194ed25fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
